28e920ba0e75b1674bff370d627d26b1756b0ec1
OTAVIO NICOLAU DA SILVA <otavio.nicollau@gmail.com>
2024-09-23 13:46:52 -0300

    ajustes

--- EDITAL/DIREITOS HUMANOS E CIDADANIA.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO assuntos (id, materia, bloco, peso, nome, hierarquia, subTree, edital) VALUES
-(1401, 'DIREITOS HUMANOS E CIDADANIA', 'III', 0, '1 Direitos humanos na Constituição Federal.', '1', 0, 'PRF'),
-(1402, 'DIREITOS HUMANOS E CIDADANIA', 'III', 0, '1.1 A Constituição Federal e os tratados internacionais de direitos humanos.', '1.1', 1401, 'PRF'),
-(1403, 'DIREITOS HUMANOS E CIDADANIA', 'III', 0, '2 Declaração Universal dos Direitos Humanos.', '2', 0, 'PRF'),
-(1404, 'DIREITOS HUMANOS E CIDADANIA', 'III', 0, '3 Convenção Americana sobre Direitos Humanos (Decreto nº 678/1992).', '3', 0, '');
